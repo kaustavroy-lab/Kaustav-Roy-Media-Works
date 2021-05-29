f8566218252829b7e78443085d9c1809d6e6706b
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-05-29 11:24:31 +0530

    locker

--- digitallocker.js
+++ digitallocker.js
@@ -109,28 +109,3 @@
       });
       $('img[data-src]').lazyload();
    });
-   
-   $(document).ready(function()
-   {
-      function waitForItUpdate()
-      {
-         // change the date here
-         var dateFuture = new Date("June 01, 2021 12:00:00");
-         var dateNow = new Date();
-         var seconds = Math.floor((dateFuture - (dateNow))/1000);
-         var minutes = Math.floor(seconds/60);
-         var hours = Math.floor(minutes/60);
-         var days = Math.floor(hours/24);
-   
-         hours = Math.round(hours-(days*24));
-         minutes = Math.round(minutes-(days*24*60)-(hours*60));
-         seconds = Math.round(seconds-(days*24*60*60)-(hours*60*60)-(minutes*60));
-                                    
-         $('#waitForItDays').html(days);
-         $('#waitForItHours').html(hours);
-         $('#waitForItMinutes').html(minutes);
-         $('#waitForItSeconds').html(seconds);   
-      }
-      waitForItUpdate();
-      setInterval(waitForItUpdate, 1000);
-   });
