590acaa84cb61823955dd16d7b45439a08e22c50
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-06-02 23:54:13 +0530

    new

--- digitallocker.js
+++ digitallocker.js
@@ -103,4 +103,9 @@
          onScrollFontAwesomeIcon11();
       });
       $('img[data-src]').lazyload();
+      if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {$('#preloader').remove();}
+   });
+   $(window).on('load', function()
+   {
+      $('#preloader').remove();
    });
